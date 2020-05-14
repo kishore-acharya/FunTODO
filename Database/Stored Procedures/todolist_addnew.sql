63defb8afff23c33b991c961721d4954ec7c0e1e
kishore acharya <acharyakishore3@gmail.com>
2020-05-14 22:54:28 +0530

    db changes for adding a new toto list

---
+++ Database/Stored Procedures/todolist_addnew.sql
@@ -0,0 +1,4 @@
+﻿create procedure todolist_addnew @username varchar, @todoitems xml
+as
+insert into dbo.TodoList (username,list_data,created_at,last_modified)
+values (@username,@todoitems,GETUTCDATE(),GETUTCDATE())
